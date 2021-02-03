075f7220ef04c309055f2007b89522767438a620
Howie-G <howieghy@qq.com>
2021-02-03 20:23:01 +0800

    add hot module

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World! (:</h1>
+      <h6>Hot config test</h6>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+//This doc is configuration of webpack
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { title } = require("process");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/main.js",
+  devServer: {
+    contentBase: path.join(__dirname, "./src/"),
+    publicPath: "/",
+    host: "127.0.0.1",
+    port: 3000,
+    stats: { colors: true },
+    hot: true, //hot start
+  },
+  entry: ["./src/main.js", "./src/dev.js"],
+  // resolve: { extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"] },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -12,9 +22,18 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules/, //To rule out 'node_modules'
         use: {
           loader: "babel-loader",
+          // options: {
+          //   //Babel escape configuration options
+          //   babelrc: false,
+          //   presets: [
+          //     require.resolve("@babel/preset-react"),
+          //     [require.resolve("@babel/preset-env"), { modules: false }],
+          //   ],
+          //   cacheDirectory: true,
+          // },
         },
       },
       {
@@ -30,6 +49,8 @@ module.exports = {
       title: "react app",
       filename: "index.html",
       template: "./src/index.html",
+      // inject: true,
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
